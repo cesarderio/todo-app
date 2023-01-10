101c043bc2182a945cde1a69320de3d4daf92e22
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-10 15:20:34 -0800

    adding to todo app for lab 32

--- src/Context/Settings/index.jsx
+++ src/Context/Settings/index.jsx
@@ -7,12 +7,9 @@ const SettingsProvider = ({ children }) => {
   const [ showComplete, setShowComplete ] = useState(false);
   const [ pageItems, setPageItems ] = useState(3);
   const [ sort, setSort ] = useState('difficulty');
-  // const [ id, setId ] = useState('Id');
-  // const [ text, setText ] = useState('Some todo Text');
-  // const [ assignedTo, setassignedTo ] = useState('Assigned To:');
-  // const [ difficulty, setDifficulty] = useState('Difficulty');
-  
-  
+
+ 
+
   const values = {
     showComplete,
     pageItems,
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { MantineProvider } from "@mantine/core";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark'}}>
+    {/* <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark'}}> */}
+    <MantineProvider>
       <ModeProvider>
         <SettingsProvider>
           <App />
